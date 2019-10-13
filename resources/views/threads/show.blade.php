1aa85e2ad7f8e3998a7a5ec5df952552cde14e82
kuduka <kuduka@gmail.com>
2019-10-13 21:42:03 +0200

    Lesson 3

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,5 +13,19 @@
             </div>
         </div>
     </div>
+    <div class="row justify-content-center">
+        @foreach ($thread->replies as $reply)
+            <div class="col-md-8">
+                <div class="card-header">
+                    <a href="#" >{{ $reply->owner->name }} </a> said {{ $reply->created_at->diffForHumans() }}
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        {{ $reply->body }}
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
