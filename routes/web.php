dda49e80eb1ccc2c503bf40c91e3e499d91aa996
kuduka <kuduka@gmail.com>
2019-10-13 21:19:36 +0200

    Initial Lesson 1/2

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/{thread}', 'ThreadsController@show');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
