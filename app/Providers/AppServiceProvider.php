f48d6d5f43c75711aece045d6013a966db2a5b88
kuduka <kuduka@gmail.com>
2019-10-20 15:14:34 +0200

    Lesson 13

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
-
+    
 namespace App\Providers;
 
+use App\Channel;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \View::share('channels', Channel::all());
     }
 }
