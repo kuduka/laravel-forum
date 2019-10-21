a3c549e3903826c9c40ab5440aac93143d8fdb2b
kuduka <kuduka@gmail.com>
2019-10-21 21:21:54 +0200

    Lesson 16

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,9 +1,9 @@
- @extends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 col-md-offset-2">
+    <div class="row">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
                     <a href="#"> {{ $thread->creator->name }} </a> posted:
@@ -13,29 +13,36 @@
                     {{ $thread->body }}
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-8 col-md-offset-2">
-            @foreach ($thread->replies as $reply)
+            
+            @foreach ($replies as $reply)
                 @include('threads.reply')
             @endforeach
+
+            {{ $replies->links() }}
+
+            @if (auth()->check())
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        @csrf
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="has something to say?" rows="5"> </textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Post</button>
+                        </form>
+            @else
+                <p class="text-center"> Please <a href="{{ route('login') }}"> Sign in</a> to participate in this discussion.</p>
+            @endif
         </div>
-    </div>
-    @if (auth()->check())
-        <div class="row justify-content-center">
-            <div class="col-md-8 col-md-offset-2">
-                <form method="POST" action="{{ $thread->path() . '/replies' }}">
-                @csrf
-                <div class="form-group">
-                    <textarea name="body" id="body" class="form-control" placeholder="has something to say?" rows="5"> </textarea>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <p>
+                        This thread was published {{ $thread->created_at->diffForHumans() }} by
+                        <a href="#">{{ $thread->creator->name }}</a>, and currently
+                        has {{ $thread->replies_count }} {{ Str::plural('comment', $thread->replies_count) }}.
+                    </p>
                 </div>
-                <button type="submit" class="btn btn-primary">Post</button>
-                </form>
             </div>
         </div>
-    @else
-        <p class="text-center"> Please <a href="{{ route('login') }}"> Sign in</a> to participate in this discussion.</p>
-    @endif
+    </div>
 </div>
 @endsection
