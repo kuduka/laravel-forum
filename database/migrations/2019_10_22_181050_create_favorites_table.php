3810f852206c276f9e11188952edee85d75f11b7
kuduka <kuduka@gmail.com>
2019-10-22 20:39:57 +0200

    Lesson 18

---
+++ database/migrations/2019_10_22_181050_create_favorites_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')
+                ->unsigned();
+            $table->bigInteger('favorited_id')
+                ->unsigned();
+            $table->string('favorited_type');
+            $table->timestamps();
+
+            $table->unique(['user_id', 'favorited_type', 'favorited_type']);  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
