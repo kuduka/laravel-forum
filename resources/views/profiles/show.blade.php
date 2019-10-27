7fff89aec4ac9a0c622730c981043532893b868e
kuduka <kuduka@gmail.com>
2019-10-27 18:30:49 +0100

    Lesson 26-27

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -7,28 +7,14 @@
                 <div class="pb-2 mt-4 mb-2 border-bottom">
                     <h1>
                         {{ $profileUser->name }}
-                        <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
                     </h1>
                 </div>
-                @foreach ($threads as $thread)
-                    <div class="mt-3 card">
-                        <div class="card-header">
-                            <div class="level">
-                                <span class="flex">
-                                    <a href="#"> {{ $thread->creator->name }} </a> posted:
-                                    {{ $thread->title }}
-                                </span>
-                                <span>
-                                    {{ $thread->created_at->diffForHumans() }} 
-                                </span>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            {{ $thread->body }}
-                        </div>
-                    </div>
+                @foreach ($activities as $date => $activity)
+                    <h3 class="page-header">{{ $date }}</h3>
+                    @foreach ($activity as $record)
+                        @include ("profiles.activities.{$record->type}", ['activity' => $record])
+                    @endforeach
                 @endforeach
-                {{ $threads->links() }}
             </div>
         </div>
     </div>
