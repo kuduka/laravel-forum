a76bc8b2f5568e22af44b558621f32c6ba02d2b1
kuduka <kuduka@gmail.com>
2019-11-03 20:19:17 +0100

    Lesson 36

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,13 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+window.Vue.prototype.authorize = function (handler) {
+
+	let user = window.App.user;
+
+	return user ? handler(user) : false;
+};
+
 window.events = new Vue();
 
 window.flash = function (message) {
@@ -9,7 +16,8 @@ window.flash = function (message) {
 };
 
 Vue.component('flash', require('./components/Flash.vue').default);
-Vue.component('reply', require('./components/Reply.vue').default);
+Vue.component('thread-view', require('./pages/Thread.vue').default);
+
 
 const app = new Vue({
     el: '#app',
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,59 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    <div class="level">
-                        <span class="flex">
-                            <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                            <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                        </span>
-                        @can ('update', $thread)
-                            <form method="POST" action="{{ $thread->path() }}">
-                                @csrf
-                                {{ method_field('DELETE') }}
-                                <button type="submit" class="btn btn-link">Delete Thread</button>
-                            </form>
-                        @endcan
+<thread-view :initial-replies-count="{{ $thread->replies_count }}" inline-template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="level">
+                            <span class="flex">
+                                <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
+                                <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                            </span>
+                            @can ('update', $thread)
+                                <form method="POST" action="{{ $thread->path() }}">
+                                    @csrf
+                                    {{ method_field('DELETE') }}
+                                    <button type="submit" class="btn btn-link">Delete Thread</button>
+                                </form>
+                            @endcan
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        {{ $thread->body }}
                     </div>
                 </div>
-                <div class="card-body">
-                    {{ $thread->body }}
-                </div>
+                
+                <replies :data="{{ $thread->replies }}" @removed="repliesCount--"></replies>
+
+                @if (auth()->check())
+                        <form class="mt-3" method="POST" action="{{ $thread->path() . '/replies' }}">
+                            @csrf
+                            <div class="form-group">
+                                <textarea name="body" id="body" class="form-control" placeholder="has something to say?" rows="5"> </textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Post</button>
+                            </form>
+                @else
+                    <p class="text-center"> Please <a href="{{ route('login') }}"> Sign in</a> to participate in this discussion.</p>
+                @endif
             </div>
-            
-            @foreach ($replies as $reply)
-                @include('threads.reply')
-            @endforeach
-
-            {{ $replies->links() }}
-
-            @if (auth()->check())
-                    <form class="mt-3" method="POST" action="{{ $thread->path() . '/replies' }}">
-                        @csrf
-                        <div class="form-group">
-                            <textarea name="body" id="body" class="form-control" placeholder="has something to say?" rows="5"> </textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Post</button>
-                        </form>
-            @else
-                <p class="text-center"> Please <a href="{{ route('login') }}"> Sign in</a> to participate in this discussion.</p>
-            @endif
-        </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <p>
-                        This thread was published {{ $thread->created_at->diffForHumans() }} by
-                        <a href="#">{{ $thread->creator->name }}</a>, and currently
-                        has {{ $thread->replies_count }} {{ Str::plural('comment', $thread->replies_count) }}.
-                    </p>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <p>
+                            This thread was published {{ $thread->created_at->diffForHumans() }} by
+                            <a href="#">{{ $thread->creator->name }}</a>, and currently
+                            has <span v-text="repliesCount"></span> {{ Str::plural('comment', $thread->replies_count) }}.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</thread-view>
 @endsection
