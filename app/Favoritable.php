6d2d0a444a85c6d034c753514a06fbd221c41709
kuduka <kuduka@gmail.com>
2019-11-03 10:38:46 +0100

    Lesson 35

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -5,6 +5,14 @@ namespace App;
 trait Favoritable
 {
 
+
+    protected static function bootFavoritable()
+    {
+        static::deleting(function ($model) {
+            $model->favorites->each->delete();
+        });
+    }
+
     public function favorites()
     {
     	return $this->morphMany(Favorite::class, 'favorited');
@@ -23,7 +31,7 @@ trait Favoritable
     {
         $attributes = ['user_id' => auth()->id()];
 
-        $this->favorites()->where($attributes)->delete();
+        $this->favorites()->where($attributes)->get()->each->delete();
     }
 
     public function isFavorited()
