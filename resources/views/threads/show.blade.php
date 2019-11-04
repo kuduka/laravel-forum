4b01dbca2588d8a94073a25d3837d0918139c54d
kuduka <kuduka@gmail.com>
2019-11-04 16:39:19 +0100

    Lesson 37

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -26,19 +26,10 @@
                     </div>
                 </div>
                 
-                <replies :data="{{ $thread->replies }}" @removed="repliesCount--"></replies>
+                <replies :data="{{ $thread->replies }}" 
+                    @added="repliesCount++"
+                    @removed="repliesCount--"></replies>
 
-                @if (auth()->check())
-                        <form class="mt-3" method="POST" action="{{ $thread->path() . '/replies' }}">
-                            @csrf
-                            <div class="form-group">
-                                <textarea name="body" id="body" class="form-control" placeholder="has something to say?" rows="5"> </textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Post</button>
-                            </form>
-                @else
-                    <p class="text-center"> Please <a href="{{ route('login') }}"> Sign in</a> to participate in this discussion.</p>
-                @endif
             </div>
             <div class="col-md-4">
                 <div class="card">
