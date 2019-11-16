4953b5572fa03a37cbeef4c2c7ec134ce46e7ddb
kuduka <kuduka@gmail.com>
2019-11-16 12:24:28 +0100

    Lesson 40

---
+++ database/migrations/2019_11_16_105948_create_thread_subscriptions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thread_subscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')
+                ->unsigned()->index();
+            $table->bigInteger('thread_id')
+                ->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('thread_id')
+                ->references('id')
+                ->on('threads')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('thread_subscriptions');
+    }
+}
