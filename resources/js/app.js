48660cc18296187f828c8bad666cb7d8481eee26
kuduka <kuduka@gmail.com>
2019-11-18 18:33:26 +0100

    Lesson 52

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,8 +11,8 @@ window.Vue.prototype.authorize = function (handler) {
 
 window.events = new Vue();
 
-window.flash = function (message) {
-	window.events.$emit('flash', message);
+window.flash = function (message, level = 'success') {
+	window.events.$emit('flash', { message, level });
 };
 
 Vue.component('flash', require('./components/Flash.vue').default);
