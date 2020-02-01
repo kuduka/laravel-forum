2a8587e2b1ed525a5e404fc422b375ef960b395b
kuduka <kuduka@gmail.com>
2020-02-01 17:12:11 +0100

    Lesson 56

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Reply;
 use App\Thread;
+use App\User;
 use App\Http\Requests\CreatePostRequest;
+use App\Notifications\YouWereMentioned;
 use Illuminate\Http\Request;
 
 class RepliesController extends Controller
@@ -21,10 +23,23 @@ class RepliesController extends Controller
 
     public function store($channelId, Thread $thread, Request $request, CreatePostRequest $form)
     {
-        return $thread->addReply([
+        $reply = $thread->addReply([
             'body' => request('body'),
             'user_id' => auth()->id()
-        ])->load('owner');
+        ]);
+
+        preg_match_all('/\@([^\s\.]+)/', $reply->body, $matches);
+
+        $names = $matches[1];
+
+        foreach($names as $name) {
+            $user = User::whereName($name)->first();
+            if ($user) {
+                $user->notify(new YouWereMentioned($reply));
+            }
+        }
+
+        return $reply->load('owner');
     }
 
     public function destroy(Reply $reply)
