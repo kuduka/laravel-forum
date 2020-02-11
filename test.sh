27b8d2251396d1b7a4edb31cd062ec75a92c4a89
kuduka <kuduka@gmail.com>
2020-02-11 20:16:51 +0100

    Teleport update

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+redis-cli flushall
 cp .env.testing .env
 php artisan config:cache
 phpunit
 cp .env.backup .env
+php artisan config:clear
 php artisan config:cache
+php artisan cache:clear
+php artisan view:clear 
